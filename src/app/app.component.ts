e609e06df300888baf8e47f4d0899bd4698d9c39
siqizheng <siqizheng1@gmail.com>
2020-08-08 16:31:06 -0700

    Add ng-bootstrap support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
+  images = [943, 944, 984].map((n) => `https://picsum.photos/id/${n}/1920/1080`);
+  constructor(){
+  }
 }

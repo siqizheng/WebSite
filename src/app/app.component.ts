68e8f5ad0bb697768f3c1f500047a83b32f94803
siqizheng <siqizheng1@gmail.com>
2020-08-08 17:29:24 -0700

    Refine content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
-  images = [943, 944, 984].map((n) => `https://picsum.photos/id/${n}/1920/1080`);
+  images = [911, 910, 985].map((n) => `https://picsum.photos/id/${n}/1920/540`);
   constructor(){
   }
 }
